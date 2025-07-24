a03c4e4b06aa43a4a4a44e3f2b28fc9f87589fac
bianca.jesus <bianca.jesus@SWI-N1510>
2025-07-24 16:20:40 -0300

    Adicionar arquivos de projeto.

---
+++ Alura.LeilaoOnline.Selenium/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Xunit;
+
+
+namespace Alura.LeilaoOnline.Selenium
+{
+    public class UnitTest1
+    {
+
+        [Fact]
+        public void Test1()
+        {
+            //arrange 
+
+            //IWebDriver driver = new DriverManager().SetUpDriver(new ChromeConfig());
+
+            new DriverManager().SetUpDriver(new ChromeConfig());
+            IWebDriver driver = new ChromeDriver();
+
+            //act
+            driver.Navigate().GoToUrl("http://localhost:5000");
+
+            //assert
+            Assert.Contains("Leilões", driver.Title);
+        }
+    }
+}

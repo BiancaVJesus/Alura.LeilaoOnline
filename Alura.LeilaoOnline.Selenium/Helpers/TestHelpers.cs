ee885a089193f7826d0bcc1942f255eee5ede993
bianca.jesus <bianca.jesus@SWI-N1510>
2025-07-25 08:11:21 -0300

    Compartilhando recursos entre testes - primeira parte.

---
+++ Alura.LeilaoOnline.Selenium/Helpers/TestHelpers.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace Alura.LeilaoOnline.Selenium.Helpers
+{
+    public static class TestHelpers
+    {
+        public static string PastaDoExecutavel => new DriverManager().SetUpDriver(new ChromeConfig());
+       
+    }
+}
--- Alura.LeilaoOnline.Selenium/UnitTest1.cs
+++ Alura.LeilaoOnline.Selenium/UnitTest1.cs
@@ -3,28 +3,47 @@ using WebDriverManager.DriverConfigs.Impl;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using Xunit;
+using Alura.LeilaoOnline.Selenium.Helpers;
 
 
 namespace Alura.LeilaoOnline.Selenium
 {
-    public class UnitTest1
+    public class AoNavegarParaHome
     {
+        //Em C#, [Fact] é um atributo usado no framework de testes xUnit. Ele serve para marcar um método como um teste unitário que não recebe parâmetros.
 
         [Fact]
-        public void Test1()
+        public void DadoChromeAbertoDeveMostrarLeiloesNoTitulo()
         {
             //arrange 
 
             //IWebDriver driver = new DriverManager().SetUpDriver(new ChromeConfig());
 
-            new DriverManager().SetUpDriver(new ChromeConfig());
-            IWebDriver driver = new ChromeDriver();
+            IWebDriver driver = new ChromeDriver(TestHelpers.PastaDoExecutavel);
 
             //act
             driver.Navigate().GoToUrl("http://localhost:5000");
 
             //assert
             Assert.Contains("Leilões", driver.Title);
+        }
+
+
+
+
+        public void DadoChromeAbertoDeveMostrarProximosLeiloesNaPagina()
+        {
+            //arrange 
+
+            //IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));.SetUpDriver(new ChromeConfig());
+
+            IWebDriver driver = new ChromeDriver(TestHelpers.PastaDoExecutavel);
+
+            //act
+            driver.Navigate().GoToUrl("http://localhost:5000");
+
+            //assert
+            Assert.Contains("Próximos Leilões", driver.PageSource);
         }
     }
 }
